8de8628c48bb4707930fa35641c227bb2ff621cc
OlgaZueva <ozzii14@gmail.com>
2017-10-16 14:11:29 +0300

    20171016_1411

--- src/test/java/TietoRus/CountsTests/DimCounts.java
+++ src/test/java/TietoRus/CountsTests/DimCounts.java
@@ -33,16 +33,18 @@ public class DimCounts {
 
 
     @Test(enabled = true)
-    public void dimCustomers_matchData() throws SQLException, IOException {
+    public void dimFileLiner() throws SQLException, IOException {
         getPropertiesFile();
+        int countRowInDV = getCountRowInDV(properties.getProperty("fileLiner.dwh.CountRows"));
+        int countRowInDim = getCountRowInDM(properties.getProperty("fileLiner.dim.CountRows"));
+        assertRowCount(countRowInDV, countRowInDim);
+    }
 
-        int countRowsInSource = Integer.parseInt(properties.getProperty("system.RownumPool"));
-        ArrayList arrayRows = getArray(countRowsInSource);
-
-
-
-        int countRowInDV = getCountRowInDV(properties.getProperty("customers.dwh.CountRows"));
-        int countRowInDim = getCountRowInDM(properties.getProperty("customers.dim.CountRows"));
+    @Test(enabled = true)
+    public void dimControllingOffice() throws SQLException, IOException {
+        getPropertiesFile();
+        int countRowInDV = getCountRowInDV(properties.getProperty("controllingOffice.dwh.CountRows"));
+        int countRowInDim = getCountRowInDM(properties.getProperty("controllingOffice.dim.CountRows"));
         assertRowCount(countRowInDV, countRowInDim);
     }
 
@@ -79,46 +81,7 @@ public class DimCounts {
         return countRowHub;
     }
 
-    public ArrayList getArray(int countRowsInTable) throws IOException {
-        getPropertiesFile();
-        int percent = Integer.parseInt(properties.getProperty("system.PercentOfRows"));
-        int countRowsForMatch = (countRowsInTable * percent) / 100;
-        int increment = countRowsInTable / countRowsForMatch;
-        ArrayList arrayRows = new ArrayList();
-        for (int i = 1; i < (countRowsInTable - increment); i = i + increment) {
-            arrayRows.add(i);
-        }
-        System.out.println("Кол-во записей пула, которые будут сравниваться: " + arrayRows.size());
-        return arrayRows;
-    }
 
-    public void matchMaps(Map<String, Object> mapDV, Map<String, Object> mapDM) {
-        System.out.println("Map from DV = " + mapDV);
-        System.out.println("Map from DM = " + mapDM);
-
-        if (mapDM.size() == 0) {
-            System.err.println("Record in DataMart not found!");
-        } else {
-            for (Map.Entry entry : mapDV.entrySet()) {
-                Object q1 = entry.getKey();
-                Object q2 = entry.getValue();
-                if (q2 == null) {
-                    if (mapDM.get(q1) != null) {
-                        System.err.println("Column [" + q1 + "] expected null but was [" + mapDM.get(q1).toString() + "]");
-                    } else if (!mapDM.keySet().contains(q1)) {
-                        System.err.println("Column [" + q1 + "] not exist");
-                    }
-                } else {
-                    if (!q2.equals(mapDM.get(q1))) {
-                        Object secondValue = mapDM.get(q1);
-                        if (!q2.toString().equals(secondValue != null ? secondValue.toString() : null)) {
-                            System.err.println("Column [" + q1.toString() + "] does not match. Expected [" + q2 + "], actual - [" + mapDM.get(q1) + "]");
-                        }
-                    }
-                }
-            }
-        }
-    }
 }
 
 
---
+++ src/test/java/TietoRus/CountsTests/DimsDataMatching.java
@@ -0,0 +1,165 @@
+package TietoRus.CountsTests;
+
+import TietoRus.system.helpers.helpers.DBHelper;
+import org.testng.annotations.Test;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+public class DimsDataMatching {
+    private Properties properties = new Properties();
+    private DBHelper db = new DBHelper();
+    private Map<String, Object> mapFromDV = new HashMap<String, Object>();
+    private Map<String, Object> mapFromDM = new HashMap<String, Object>();
+
+
+    @Test(enabled = true)
+    public void dimCustomers_matchData() throws SQLException, IOException {
+        getPropertiesFile();
+
+        int countRowInDV = getCountRowInDV(properties.getProperty("customers.dwh.CountRows"));
+        ArrayList arrayRows = getArray(countRowInDV);
+
+        for (int i = 0; i < arrayRows.size(); i++) {
+            String sqlFromDV = (properties.getProperty("customers.dataInDV.RowByRowNum") + arrayRows.get(i));
+            System.out.println("sqlFromDV: " + sqlFromDV);
+            Connection connectionToDWH = db.connToDWH();
+            Statement stForDWH = db.stFromConnection(connectionToDWH);
+            ResultSet rsFromDWH = db.rsFromDB(stForDWH, sqlFromDV);
+            while (rsFromDWH.next()) {
+                mapFromDV = getMapFromDV(rsFromDWH);
+                String sqlForDM = (properties.getProperty("customers.dataInDM.RowByKeys") + " where dwhIdHubCustomers = " +
+                        rsFromDWH.getInt("dwhIdHubCustomers") + " and validFrom = '" + rsFromDWH.getString("validFrom") + "\'");
+                System.out.println("sqlForDM: " + sqlForDM);
+                mapFromDM = getMapFromDM(mapFromDV.size(), sqlForDM);
+            }
+            db.closeConnecions(rsFromDWH, stForDWH, connectionToDWH);
+            matchMaps(mapFromDV, mapFromDM);
+        }
+
+    }
+
+    @Test(enabled = true)
+    public void dimFileLiner_matchData() throws SQLException, IOException {
+        getPropertiesFile();
+
+        int countRowInDV = getCountRowInDV(properties.getProperty("fileLiner.dwh.CountRows"));
+        ArrayList arrayRows = getArray(countRowInDV);
+
+        for (int i = 0; i < arrayRows.size(); i++) {
+            String sqlFromDV = (properties.getProperty("fileLiner.dataInDV.RowByRowNum") + arrayRows.get(i));
+            System.out.println("sqlFromDV: " + sqlFromDV);
+            Connection connectionToDWH = db.connToDWH();
+            Statement stForDWH = db.stFromConnection(connectionToDWH);
+            ResultSet rsFromDWH = db.rsFromDB(stForDWH, sqlFromDV);
+            while (rsFromDWH.next()) {
+                mapFromDV = getMapFromDV(rsFromDWH);
+                String sqlForDM = (properties.getProperty("fileLiner.datainDM.RowByKeys") + " where dwhIdHubFileLiner = " +
+                        rsFromDWH.getInt("dwhIdHubFileLiner") + " and validFrom = '" + rsFromDWH.getString("validFrom") + "\'");
+                System.out.println("sqlForDM: " + sqlForDM);
+                mapFromDM = getMapFromDM(mapFromDV.size(), sqlForDM);
+            }
+            db.closeConnecions(rsFromDWH, stForDWH, connectionToDWH);
+            matchMaps(mapFromDV, mapFromDM);
+        }
+
+    }
+
+    private void getPropertiesFile() throws IOException {
+        properties.load(new FileReader(new File(String.format("src/test/resources/dimsCountsSQL.properties"))));
+    }
+
+    public int getCountRowInDV(String hubSQL) throws SQLException {
+        Connection connectionToDWH = db.connToDWH();
+        Statement stForDWH = db.stFromConnection(connectionToDWH);
+        ResultSet rsFromDWH = db.rsFromDB(stForDWH, hubSQL);
+        int countRowHub = 0;
+        while (rsFromDWH.next()) {
+            countRowHub = Integer.parseInt(rsFromDWH.getString("c"));
+        }
+        db.closeConnecions(rsFromDWH, stForDWH, connectionToDWH);
+        return countRowHub;
+    }
+
+    public ArrayList getArray(int countRowsInTable) throws IOException {
+        getPropertiesFile();
+        Double percent = Double.valueOf((properties.getProperty("system.PercentOfRows")));
+        int countRowsForMatch = (int) Math.round(((countRowsInTable * percent) / 100));
+        int increment = Math.round(countRowsInTable / countRowsForMatch);
+        ArrayList arrayRows = new ArrayList();
+        for (int i = 1; i < (countRowsInTable - increment); i = i + increment) {
+            arrayRows.add(i);
+        }
+        System.out.println("Кол-во записей пула, которые будут сравниваться: " + arrayRows.size());
+        return arrayRows;
+    }
+
+    public Map<String, Object> getMapFromDV(ResultSet rsFromSource) throws SQLException {
+        for (int k = 1; k <= rsFromSource.getMetaData().getColumnCount(); k++) {
+            mapFromDV.put(rsFromSource.getMetaData().getColumnName(k), rsFromSource.getObject(k));
+            mapFromDV.remove("RowNumber");
+        }
+        return mapFromDV;
+    }
+
+    public Map<String, Object> getMapFromDM(int mapForRTestSize, String sql) throws SQLException {
+        Connection connectionToDM = db.connToDM();
+        Statement stForDM = db.stFromConnection(connectionToDM);
+        ResultSet rsFromDM = db.rsFromDB(stForDM, sql);
+        Map<String, Object> mapForDM = new HashMap<String, Object>();
+
+        while (rsFromDM.next()) {
+            if (rsFromDM.getRow() > 1) {
+                System.err.println("Count rows got from DM: " + rsFromDM.getRow()
+                        + ". If its > 1 check the unique key in sql query to DM! SQL: " + sql);
+            } else {
+                for (int l = 1; l <= mapForRTestSize; l++) {
+                    mapForDM.put(rsFromDM.getMetaData().getColumnName(l), rsFromDM.getObject(l));
+                }
+            }
+        }
+        db.closeConnecions(rsFromDM, stForDM, connectionToDM);
+        return mapForDM;
+    }
+
+
+    public void matchMaps(Map<String, Object> mapDV, Map<String, Object> mapDM) {
+        System.out.println("Map from DV = " + mapDV);
+        System.out.println("Map from DM = " + mapDM);
+
+        if (mapDM.size() == 0) {
+            System.err.println("Record in DataMart not found!");
+        } else {
+            for (Map.Entry entry : mapDV.entrySet()) {
+                Object q1 = entry.getKey();
+                Object q2 = entry.getValue();
+                if (q2 == null) {
+                    if (mapDM.get(q1) != null) {
+                        System.err.println("Column [" + q1 + "] expected null but was [" + mapDM.get(q1).toString() + "]");
+                    } else if (!mapDM.keySet().contains(q1)) {
+                        System.err.println("Column [" + q1 + "] not exist");
+                    }
+                } else {
+                    if (!q2.equals(mapDM.get(q1))) {
+                        Object secondValue = mapDM.get(q1);
+                        if (!q2.toString().equals(secondValue != null ? secondValue.toString() : null)) {
+                            System.err.println("Column [" + q1.toString() + "] does not match. Expected [" + q2 + "], actual - [" + mapDM.get(q1) + "]");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+
