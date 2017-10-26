1eb08919c95dbf61615d5ce8383c27f54a18b318
OlgaZueva <ozzii14@gmail.com>
2017-10-26 13:34:21 +0300

    20171026_1334

--- src/test/java/TietoRus/CountsTests/LinksCounts.java
+++ src/test/java/TietoRus/CountsTests/LinksCounts.java
@@ -2162,6 +2162,23 @@ public class LinksCounts {
         assertRowCount(countRowInLink, countRowInSatLink);
     }
 
+    @Test(enabled = true)
+    public void LoopSearch_lnkLoopSearch() throws SQLException, IOException {
+        getPropertiesFile();
+        System.err.println("Линк специфичен. Проверить глазами. В satLnk записываются поля из src-таблицы. В lnk -  два dwhIdHub и два значения из полей. countryCode и locationCode ");
+        int countRowByCondition = getCountRowOfHub(properties.getProperty("lnkLoopSearch.condition.CountRows"));
+        int countRowInLink = getCountRowOfHub(properties.getProperty("lnkLoopSearch.lnk.CountRows"));
+        assertRowCount(countRowByCondition, countRowInLink);
+    }
+
+    @Test(enabled = true)
+    public void LoopSearch_satLnkLoopSearch() throws SQLException, IOException {
+        getPropertiesFile();
+        int countRowInLink = getCountRowOfHub(properties.getProperty("lnkLoopSearch.lnk.CountRows"));
+        int countRowInSatLink = getCountRowOfHub(properties.getProperty("lnkLoopSearch.satLnk.CountRows"));
+        assertRowCount(countRowInLink, countRowInSatLink);
+    }
+
 
 
 
