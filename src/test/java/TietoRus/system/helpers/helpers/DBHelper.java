25e3c1b038635d73c7c2dbde76958a1cc15dbe6f
OlgaZueva <ozzii14@gmail.com>
2018-03-07 00:44:20 +0300

    20180307_0044

--- src/test/java/TietoRus/system/helpers/helpers/DBHelper.java
+++ src/test/java/TietoRus/system/helpers/helpers/DBHelper.java
@@ -20,27 +20,27 @@ public class DBHelper {
     public Connection connToDM() throws SQLException {
 
          //return DriverManager.getConnection("jdbc:sqlserver://10.45.101.96;databaseName=DataMart;user=sa;password=JGdedf&#fsqwhdc");
-       // return DriverManager.getConnection("jdbc:sqlserver://10.45.1.122;databaseName=DataMartTest;user=ssis;password=ssis");
+        return DriverManager.getConnection("jdbc:sqlserver://10.45.1.122;databaseName=DataMartTestNew;user=ssis;password=ssis");
        // return DriverManager.getConnection("jdbc:sqlserver://10.45.1.122;databaseName=DataMartTestCh;user=ssis;password=ssis");
         //return DriverManager.getConnection("jdbc:sqlserver://10.21.11.15;databaseName=DataMartTest;user=ssis;password=ssis");
-       return DriverManager.getConnection("jdbc:sqlserver://10.21.11.11;databaseName=DataMartTest;user=ssis;password=ssis");
+       //return DriverManager.getConnection("jdbc:sqlserver://10.21.11.11;databaseName=DataMartTest;user=ssis;password=ssis");
         //return DriverManager.getConnection("jdbc:sqlserver://10.45.101.96;databaseName=DataMartTest;user=sa;password=JGdedf&#fsqwhdc");
     }
 
     public Connection connToSA() throws SQLException {
 
        //  return DriverManager.getConnection("jdbc:sqlserver://10.45.101.96;databaseName=StagingArea;user=sa;password=JGdedf&#fsqwhdc");
-       // return DriverManager.getConnection("jdbc:sqlserver://10.45.1.122;databaseName=StagingAreaTest;user=ssis;password=ssis");
+        return DriverManager.getConnection("jdbc:sqlserver://10.45.1.122;databaseName=StagingAreaTest;user=ssis;password=ssis");
         //return DriverManager.getConnection("jdbc:sqlserver://10.21.11.15;databaseName=StagingAreaTest;user=ssis;password=ssis");
-        return DriverManager.getConnection("jdbc:sqlserver://10.21.11.11;databaseName=StagingAreaTest;user=ssis;password=ssis");
+       // return DriverManager.getConnection("jdbc:sqlserver://10.21.11.11;databaseName=StagingAreaTest;user=ssis;password=ssis");
         //return DriverManager.getConnection("jdbc:sqlserver://10.45.101.96;databaseName=StagingArea;user=sa;password=JGdedf&#fsqwhdc");
     }
 
     public Connection connToDWH() throws SQLException {
          //return DriverManager.getConnection("jdbc:sqlserver://10.45.101.96;databaseName=DataVault;user=sa;password=JGdedf&#fsqwhdc");
-       // return DriverManager.getConnection("jdbc:sqlserver://10.45.1.122;databaseName=DataVaultTest;user=ssis;password=ssis");
+        return DriverManager.getConnection("jdbc:sqlserver://10.45.1.122;databaseName=DataVaultTest;user=ssis;password=ssis");
         // return DriverManager.getConnection("jdbc:sqlserver://10.21.11.15;databaseName=DataVaultTest;user=ssis;password=ssis");
-        return DriverManager.getConnection("jdbc:sqlserver://10.21.11.11;databaseName=DataVaultTest;user=ssis;password=ssis");
+       // return DriverManager.getConnection("jdbc:sqlserver://10.21.11.11;databaseName=DataVaultTest;user=ssis;password=ssis");
         // return DriverManager.getConnection("jdbc:sqlserver://10.45.101.96;databaseName=DataVault;user=sa;password=JGdedf&#fsqwhdc");
 
     }
